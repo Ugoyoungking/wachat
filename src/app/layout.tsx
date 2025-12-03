774dd70894e9b9ba63fc8916570f123088aa9853
Ugochukwu Jonathan <ugochukwujonathan067@gmail.com>
2025-12-03 22:15:56 +0000

    also make it pwa friendly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -197,6 +197,12 @@ export default function RootLayout({
         <meta name="google-site-verification" content="W3MpZ-n3f__nszkbbn7M_8K2F8fttcYJTqwkJrwfX8o" />
         <meta name="msvalidate.01" content="63A610B3C9552E33F88103CB9AD8CF70" />
         <meta name="theme-color" content="#0A7CFF" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="WaChat" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="WaChat" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="alternate" hrefLang="en" href="https://wachat-app.vercel.app/" />
         <script
             type="application/ld+json"
